32f4b1d8947a9d42896614eba4317b2af554ad29
vagrant <vagrant@homestead>
2026-03-17 14:40:53 +0000

    Customized footer and news section

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Sulu\Bundle\MediaBundle\Media\Manager\MediaManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Manuxi\SuluNewsBundle\Repository\NewsRepository;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class NewsController extends AbstractController
 {
@@ -22,4 +25,41 @@ class NewsController extends AbstractController
 
         return new Response('<img src="' . $url . '" alt="Hero Image">');
     }
+
+
+    /**
+     * Show recent posts (latest 5, excluding current one if provided)
+     */
+public function recent(NewsRepository $repository, Request $request, ?int $excludeId = null): Response
+{
+    $locale = $request->getLocale();
+
+    $qb = $repository->createQueryBuilder('n')
+        ->select('DISTINCT n')
+        ->leftJoin('n.translations', 'translation')
+        ->where('translation.locale = :locale')
+        ->andWhere('translation.published = :published')
+        ->andWhere('n.type != :excludedType') // exclude type announcement
+        ->setParameter('locale', $locale)
+        ->setParameter('published', true)
+        ->setParameter('excludedType', 'announcement')
+        ->orderBy('translation.authored', 'DESC')
+        ->setMaxResults(5);
+
+    if ($excludeId) {
+        $qb->andWhere('n.id != :id')->setParameter('id', $excludeId);
+    }
+
+    $recentPosts = $qb->getQuery()->getResult();
+
+    return $this->render('news/recent.html.twig', [
+        'recentPosts' => $recentPosts,
+    ]);
+}
+
+
+
+
+
+
 }
